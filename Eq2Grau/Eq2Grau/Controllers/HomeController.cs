24f1e44ad44d643723f6b3ac2510dfccbcd287f2
Filipe Branco <estt6967@ipt.pt>
2025-03-02 22:34:05 +0000

    Construção do algoritmo da calculadora

--- Eq2Grau/Eq2Grau/Controllers/HomeController.cs
+++ Eq2Grau/Eq2Grau/Controllers/HomeController.cs
@@ -15,6 +15,25 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
+         /* ALGORITMO
+          * 1- ler parâmetros a, b, c
+          * 2- verificar se os parâmetros são números
+          *    se não, criar msg aviso
+          *    se sim, continuo
+          * 3- a =/= 0 ????
+          *    se não, mostro msg de aviso
+          *    se sim, continuo
+          * 4- calcular DELTA = b2-4ac
+          *    4.1- se DELTA > 0, calcular raízes reais
+          *         x1 = (-b - sqrt(DELTA))/2/a
+          *         x2 = (-b + sqrt(DELTA))/2/a
+          *    4.2- se DELTA = 0
+          *         x1 = x2 = (-b)/2/a
+          *    4.3- se DELTA < 0, calcular raízes complexas, conjugadas
+          *         x1 = (-b)/2/a '+' sqrt(-DELTA))/2/a 'i'
+          *         x2 = (-b)/2/a '-' sqrt(-DELTA))/2/a 'i'
+          * 5- mostrar o resultado na VIEW
+          */
         return View();
     }
 
